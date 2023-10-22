e71c8d12a1b9c13a20be3843562d0dd8bf331eb4
ons-elyahmadi <ons.elyahmadi@fsb.ucar.tn>
2023-10-22 16:33:00 +0100

    tp2

---
+++ demo/src/main/java/com/example/demo/EmployeeRepository.java
@@ -0,0 +1,5 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import net.javaguides.springboot.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ demo/src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import net.javaguides.springboot.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(long id);
+    Employee updateEmployee(Employee employee, long id);
+    void deleteEmployee(long id);
+}
